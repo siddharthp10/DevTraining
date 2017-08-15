d04a40c921a608aefaefda113b9f486950cf5584
siddharth10p <siddharth10p@gmail.com>
2017-08-15 20:24:01 +0530

    Initial checking for Apex basics
    Apex basics for developer training.

---
+++ Apex-Basics/3) Loops/For_Loop.java
@@ -0,0 +1,13 @@
+/* 2.0 traditional for loop */
+for (Integer i = 0; i < 10; i++) {
+    // Your code goes here
+}
+// Your code goes here
+
+
+/* 2.1 Advanced for loop */
+List<Account> accList = [SELECT Id, Name FROM Account LIMIT 5];
+for (Account acc : accList) {
+    // Your code goes here
+}
+// Your code goes here
